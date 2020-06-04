566cf95fd4b0e19b9a92c18e9d5badd7c9e7c951
Michael Worm <michael.worm@essquare.de>
2020-06-04 14:26:56 +0200

    Add APIs for Cars

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 import { Car, cars as cars_list } from './cars';
@@ -7,23 +7,23 @@ import { Car, cars as cars_list } from './cars';
   let cars:Car[]  = cars_list;
 
   //Create an express applicaiton
-  const app = express(); 
+  const app = express();
   //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
+  const port = 8082;
+
+  //use middleware so post bodies
   //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  app.use(bodyParser.json());
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
     res.status(200).send("Welcome to the Cloud!");
   } );
 
-  // Get a greeting to a specific person 
+  // Get a greeting to a specific person
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
+  app.get( "/persons/:name",
     ( req: Request, res: Response ) => {
       let { name } = req.params;
 
@@ -52,9 +52,9 @@ import { Car, cars as cars_list } from './cars';
 
   // Post a greeting to a specific person
   // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
+  // > try it by posting {"name": "the_name" } as
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
+  app.post( "/persons",
     async ( req: Request, res: Response ) => {
 
       const { name } = req.body;
@@ -70,13 +70,49 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let { make } = req.query;
+
+    if ( !make ) {
+      return res.status(400)
+        .send(`name is required`);
+    }
+
+    return res.status(200)
+      .send(cars.filter(car => car.make === make));
+  } );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id", ( req: Request, res: Response ) => {
+    let { id } = req.query;
+
+    if ( !id ) {
+      return res.status(400)
+        .send(`name is required`);
+    }
+
+    return res.status(200)
+      .send(cars.filter(car => car.id === id));
+  })
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars",
+    async ( req: Request, res: Response ) => {
+
+      const { id, type, model, cost, make } = req.body;
+
+      if ( !id || !type || !cost || !model || !make ) {
+        return res.status(400)
+          .send(`id, type, model, cost is required`);
+      }
+
+      cars.push({id, type, cost, model, make})
+
+      return res.status(200).send(cars);
+    } );
 
   // Start the Server
   app.listen( port, () => {
